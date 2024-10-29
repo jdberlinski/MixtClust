26bd67d8bab25c8261b2cc2ca995a314c4d8706e
Josh Berlinski <jberlin@iastate.edu>
2024-10-29 12:13:27 -0400

    add function to get BIC values from list

---
+++ R/extract.R
@@ -0,0 +1,43 @@
+#' Extract BIC for list of MixtClust results
+#'
+#' @description After obtaining a list of results for various constraints and
+#' number of clusters, extract a data frame contaning the BIC for each model.
+#'
+#' @param res A list of results given as the output to \code{MixtClust()}
+#'
+#' @return A data frame with the columns "constraint", "nclusters", and "BIC"
+#'
+#' @examples
+#' set.seed(20241029)
+#' d <- subset(iris, select = -Species)
+#' res <- MixtClust(d, nclusters = 2:3, sigma.constr = "all")
+#' BICs <- extract_bic(res)
+#'
+#' @author Josh Berlinski
+#'
+#' @export
+extract_bic <- function(res) {
+  # number of clusters is nested within constraint
+  df <- Map(
+    function(constr_res) {
+      df_inner <- Map(
+        function(nc_res) {
+          if (class(nc_res) != "MixtClust")
+            stop("Results must be output from `MixtClust()`")
+
+          data.frame(
+            constraint = nc_res$sigma.constr,
+            nclusters = nc_res$nclusters,
+            BIC = nc_res$bic
+          )
+        },
+        constr_res
+      )
+      Reduce(rbind, df_inner)
+    },
+    res
+  )
+
+  Reduce(rbind, df)
+}
+
